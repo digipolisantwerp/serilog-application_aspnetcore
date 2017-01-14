d430a685fda0df76e08a9c33d73c6753198aa684
Steven Vanden Broeck <xytyx@telenet.be>
2017-01-14 17:18:12 +0100

    Startup.

---
+++ src/Digipolis.Serilog.ApplicationEnrichment/Enrichers/AddApplicationContextEnricherExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Digipolis.Serilog
+{
+    public static class AddApplicationContextEnricherExt
+    {
+        public static SerilogExtensionsOptions AddApplicationContextEnricher(this SerilogExtensionsOptions options)
+        {
+            options.AddEnricher<ApplicationContextEnricher>();
+            return options;
+        }
+    }
+}
---
+++ test/Digipolis.Serilog.ApplicationEnrichment.UnitTests/Enrichers/AddApplicationContextEnricherExtTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog.Core;
+using Xunit;
+
+namespace Digipolis.Serilog.ApplicationEnrichment.UnitTests.Enrichers
+{
+    public class AddApplicationContextEnricherExtTests
+    {
+        [Fact]
+        void ApplicationContextEnricherIsAdded()
+        {
+            var options = new SerilogExtensionsOptions();
+            options.AddApplicationContextEnricher();
+            Assert.Collection(options.EnricherTypes, item => Assert.Equal(typeof(ApplicationContextEnricher), item));
+        }
+
+        [Fact]
+        void ApplicationContextEnricherIsRegisteredAsSingleton()
+        {
+            var services = new ServiceCollection();
+            services.AddSerilogExtensions(options => {
+                options.MessageVersion = "1";
+                options.AddApplicationContextEnricher();
+            });
+
+            var registrations = services.Where(sd => sd.ServiceType == typeof(ILogEventEnricher) &&
+                                                     sd.ImplementationType == typeof(ApplicationContextEnricher))
+                                                     .ToArray();
+
+            Assert.Equal(1, registrations.Count());
+            Assert.Equal(ServiceLifetime.Singleton, registrations[0].Lifetime);
+        }
+    }
+}
